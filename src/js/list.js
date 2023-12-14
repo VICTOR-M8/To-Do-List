8005b5c9da75aabd699611ad6e337016743723a9
VICTOR-M8 <darkhink8@gmail.com>
2023-12-14 14:35:56 +0100

    new functions

--- src/js/list.js
+++ src/js/list.js
@@ -13,11 +13,103 @@ let id // para que inicie en 0 cada tarea tendra un id diferente
 //creacion de fecha actualizada 
 
 const FECHA = new Date()
-fecha.innerHTML = FECHA.toLocaleDateString('es-MX', { weekday: 'long', month: 'short', day: 'numeric' })
+fecha.innerHTML = FECHA.toLocaleDateString('es-es', { weekday: 'long', month: 'short', day: 'numeric' })
 
 
 
 
+// Lógica para editar una tarea al hacer clic en "Editar"
+// Función para habilitar la edición de las tareas al presionar "Editar"
+function habilitarEdicion() {
+    const tasks = document.querySelectorAll('#lista li .text');
+
+    tasks.forEach(task => {
+        task.setAttribute('contenteditable', 'true');
+        task.addEventListener('blur', function () {
+            task.removeAttribute('contenteditable');
+            alert('El texto ha sido modificado: ' + task.textContent);
+        });
+    });
+}
+
+// Lógica para seleccionar una tarea al hacer clic en su texto
+const tasks = document.querySelectorAll('#lista li');
+
+tasks.forEach(task => {
+    const textElement = task.querySelector('.text');
+
+    textElement.addEventListener('click', function (e) {
+        const selected = document.querySelector('.selected');
+        if (selected !== null) {
+            selected.classList.remove('selected');
+        }
+        task.classList.add('selected');
+        e.stopPropagation();
+    });
+});
+
+// Lógica para editar las tareas al hacer clic en "Editar"
+document.getElementById('editTask').addEventListener('click', function () {
+    habilitarEdicion();
+});
+//-----------------------
+
+// Lógica para ordenar alfabéticamente las tareas al hacer clic en "Ordenar"
+document.getElementById('sortTasks').addEventListener('click', function () {
+    const tasks = [...document.querySelectorAll('#lista li .text')];
+    const sortedTasks = tasks.map(task => task.textContent).sort();
+    const list = document.getElementById('lista');
+    sortedTasks.forEach((task, index) => {
+        list.children[index].querySelector('.text').textContent = task;
+    });
+});
+
+// Lógica para reordenar las tareas al hacer clic en "Reordenar"
+document.getElementById('reorderTasks').addEventListener('click', function () {
+    const tasks = document.querySelectorAll('#lista li');
+
+    tasks.forEach(task => {
+        task.draggable = true;
+
+        task.addEventListener('dragstart', function (e) {
+            e.dataTransfer.setData('text/plain', null); // Necesario para Firefox
+            e.target.classList.add('dragging');
+        });
+
+        task.addEventListener('dragend', function () {
+            const draggingTask = document.querySelector('.dragging');
+            draggingTask.classList.remove('dragging');
+        });
+    });
+
+    const list = document.getElementById('lista');
+
+    list.addEventListener('dragover', function (e) {
+        e.preventDefault();
+        const afterElement = getDragAfterElement(list, e.clientY);
+        const draggingTask = document.querySelector('.dragging');
+        if (afterElement == null) {
+            list.appendChild(draggingTask);
+        } else {
+            list.insertBefore(draggingTask, afterElement);
+        }
+    });
+});
+
+function getDragAfterElement(list, y) {
+    const draggableTasks = [...list.querySelectorAll('li:not(.dragging)')];
+    return draggableTasks.reduce((closest, task) => {
+        const box = task.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: task };
+        } else {
+            return closest;
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element;
+}
+
+// ... tu código actual ...
 
 
 
@@ -38,6 +130,16 @@ function agregarTarea(tarea, id, realizado, eliminado) {
                         </li>
                     `
     lista.insertAdjacentHTML("beforeend", elemento)
+    var taskCount = document.querySelectorAll('#taskList li').length;
+    if (taskCount > 3) {
+        var extraTasks = document.querySelectorAll('#taskList li:nth-child(n+4)');
+        var extraTasksList = document.getElementById('extraTasks');
+        extraTasks.forEach(function (task) {
+            extraTasksList.appendChild(task);
+        });
+        document.getElementById('showMoreTasks').classList.remove('hidden');
+    }
+
 
 }
 
