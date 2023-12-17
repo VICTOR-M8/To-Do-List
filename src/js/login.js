fc23a8961205d7e5e8a799353ce6075c9bc32b35
VICTOR-M8 <darkhink8@gmail.com>
2023-12-17 20:12:57 +0100

    main page and css were added

---
+++ src/js/login.js
@@ -0,0 +1,16 @@
+const loginForm = document.querySelector('#loginForm');
+        loginForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const email = document.querySelector('#email').value;
+            const password = document.querySelector('#password').value;
+            const Users = JSON.parse(localStorage.getItem('users')) || [];
+            const validUser = Users.find(user => user.email === email && user.password === password);
+            if (!validUser) {
+                alert('Usuario y/o contraseña incorrectos');
+            } else {
+                alert(`¡Bienvenido ${validUser.name}!`);
+                // Redirect to index.html
+                window.location.href = 'home.html';
+
+            }
+        });
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
-// Obtener el botón por su ID
-const boton = document.getElementById('botonRedireccionar');
+// Obtener los botones
+const loginButton = document.getElementById('loginButton');
+const signupButton = document.getElementById('signupButton');
 
-// Agregar un event listener para el clic en el botón
-boton.addEventListener('click', function() {
-    // Redireccionar a otra página
-    window.location.href = 'http://127.0.0.1:5500/src/html/list.html'; // Reemplaza con la URL deseada
+// Redirigir a las páginas respectivas al hacer clic
+loginButton.addEventListener('click', () => {
+    window.location.href = 'http://127.0.0.1:5500/src/html/login.html'; // Reemplaza 'ruta_para_login.html' con la ruta correcta de tu página de inicio de sesión
+});
+
+signupButton.addEventListener('click', () => {
+    window.location.href = 'http://127.0.0.1:5500/src/html/sign-up.html'; // Reemplaza 'ruta_para_registro.html' con la ruta correcta de tu página de registro
 });
