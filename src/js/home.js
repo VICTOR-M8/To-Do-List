2fe6daef9da80fbb2ece2cd497889099d2d31248
VICTOR-M8 <darkhink8@gmail.com>
2023-12-18 12:04:22 +0100

    added the other task tabs and css

--- src/js/home.js
+++ src/js/home.js
@@ -36,3 +36,32 @@ document.addEventListener('DOMContentLoaded', function() {
     const logoutButton = document.getElementById('logoutButton');
     logoutButton.addEventListener('click', cerrarSesion);
 });
+
+
+const cards = document.querySelectorAll('.card');
+
+
+const cardLinks = {
+    'dia': 'dia.html',
+    'importante': 'imp.html',
+    'hogar': 'hogar.html',
+    'educacion': 'edu.html'
+};
+
+
+function handleCardClick(event) {
+    
+    const cardClass = event.currentTarget.classList[1];
+    
+
+    const link = cardLinks[cardClass];
+    
+
+    if (link) {
+        window.location.href = link;
+    }
+}
+
+cards.forEach(card => {
+    card.addEventListener('click', handleCardClick);
+});
