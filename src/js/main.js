98224ae21cb8daa7ac58301336ceba738ed76a39
VICTOR-M8 <darkhink8@gmail.com>
2024-01-08 17:50:10 +0100

    new changes for deployment

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,9 @@ const signupButton = document.getElementById('signupButton');
 
 // Redirigir a las páginas respectivas al hacer clic
 loginButton.addEventListener('click', () => {
-    window.location.href = 'https://victor-m8.github.io/To-Do-List//src/html/login.html';  
+    window.location.href = 'sign-up.html';  
 });
 
 signupButton.addEventListener('click', () => {
-    window.location.href = '/src/html/sign-up.html'; 
+    window.location.href = 'login.html'; 
 });
