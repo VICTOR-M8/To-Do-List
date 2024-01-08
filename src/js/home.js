85ebf420fbaba27aa8fe08de989bfb219ddcd4dd
VICTOR-M8 <darkhink8@gmail.com>
2024-01-08 13:49:33 +0100

    new change

--- src/js/home.js
+++ src/js/home.js
@@ -5,14 +5,14 @@ fecha.innerHTML = FECHA.toLocaleDateString('es-es', { weekday: 'long', month: 's
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Agregar evento al botón de cierre de sesión
+
     const logoutButton = document.getElementById('logoutButton');
     logoutButton.addEventListener('click', cerrarSesion);
 });
 
 function cerrarSesion() {
-    localStorage.removeItem('usuario'); // Eliminar la información del usuario al cerrar sesión
-    window.location.href = 'login.html'; // Redireccionar a la página de login
+    localStorage.removeItem('usuario'); 
+    window.location.href = 'login.html'; 
 }
 
 const cards = document.querySelectorAll('.card');
